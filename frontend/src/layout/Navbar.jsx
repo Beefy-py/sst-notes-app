45cf7bde485df7ee24051fd625cff06c246a84b8
beefykenny <hoftkenny@gmail.com>
2022-05-31 12:30:10 -0300

    cta btn look

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -47,9 +47,9 @@ const Navbar = () => {
           </div>
           <button
             type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-gray-800 hover:bg-blue-700 transition focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Get More Notes
+            Buy Notes
           </button>
           <button
             data-collapse-toggle="mobile-menu-4"
